4fb11c7e9822500f628be23f37731ed7b5331966
Emmanuel <nwekeemmanuel667@gmail.com>
2026-09-26 16:18:21 +0100

    Prepare EGWM production deployment

--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,10 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
+
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
